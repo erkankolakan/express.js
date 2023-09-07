33519e4128a6182f8713558a3c87f27d8cdf4646
Erkan <erkankolakan@gmail.com>
2023-09-07 20:17:42 +0300

    html sayfalarının getirilmesi

--- app.js
+++ app.js
@@ -1,57 +1,30 @@
-// Routing işlemleri -> routing işlemleri de aslında midleware işlemlerine benzer şekilde çalışır.
-
+// HTML sayfalarının getirilmesi
 const express = require("express");
 const app = express(); 
+const path = require("path") // -> path.join([...paths]): Belirtilen yolları birleştirir ve sonuç olarak tek bir yol döndürür. path çeşitli parametreler alabilir biz bu kısımda join kullanıyoruz ama
+/* path path.resolve([...paths]): Belirtilen yolları çözümler ve mutlak bir yol döndürür. Çözümlenen yol, çalışma dizinine göre hesaplanır. Örneğin:
+const absolutePath = path.resolve('dosya', 'altDosya', 'dosya.txt'); */
 
+    app.use( "/blogs/:blogid", (req ,res, next) => {
+        //global değişkenimiz var __dirname ve __fileName şeklinde 
+        console.log(__dirname); // -> projenin bulunduğumuz klasör (ör/express.js)
+        console.log(__filename); // -> o anda bulunduğumuz klasör (ör/app.js)
 
-
-
-    app.use( "/blogs/:blogid/user/:username", (req ,res, next) => {
-//aslında biz burda /blogs/5 demek yerine /blogs/:blogid gibi bir değişken vermemiz gerekir. Bu sayede 67 nolu id ye sayif sayfay, veri tabanından sorgulayacağız ona göre bilgi yönlendirmesi yapacağız bu sayede sayfa başlığı sayfa görseli id numarasına göre yönlendireceğiz.
-// buradaki :blogid bilgisini req.params sayesinde erişeceğiz.
-// console.log(req.params); yazarsak { blogid : "56" } şeklinde gelir ama biz req.params.blogid yazarsak direk id numarasını verir.
-// blogs/:blogid/user/username -> burada blog user sabit değerlerdir aslında birşey yazmadan önce neyin geleceğini yazıyoruz gibi düşünülebilir.
-// bu sayede biz /blogs/5/user/ahmet yazarsak req.params.blogid = 5 ve req.params.username = ahmet olur.
-
-        console.log(req.params.blogid);
-        console.log(req.params.username);
-        res.send("burası blog detay sayfası")
-
+        res.sendFile(path.join(__dirname, "views/users","blogDetails.html")) //-> path.join ile dosyaları birleştiriyoruz.
+       
     }  )
 
     app.use( "/blogs", (req ,res, next) => {
-        res.send("burası blogs")
-
+        res.sendFile(path.join(__dirname, "views/users","blogs.html"))
     }  )
 
     app.use( "/", (req ,res, next) => {
-        res.send("burası anasayfa")
- 
+        res.sendFile(path.join(__dirname, "views/users","index.html"))
+//senFile ile bir dosyayı çağırabiliyoruz biz tabilide buraya manuel olarak views/users/index html şekliden bir dizilim yapabilirdik ama bu 
     }  )
-
-    /* bu hiyerarşide / sayfası en üstde olsa biz url yi değiştirksekde sürekli olarak / sayfası gözükür çünkü routes sayfası ne başa balar / varsa direk onu alır, o yüzden bizim en özel url ye sahip fonksiyonu en üste koymamız gerekir.
-    eğer biz blogs/6 diye bir çağırma yaparsak /blogs/5 karşılamayacağı için /blogs sayfasına düşer.
-    
-    
-    */
-
-
-
-
-
-
 
     app.listen(3000 , () => {
         console.log("Server started on port 3000");
     })
-
-    /*
-        routing işleminde eskiden
-        if (req.url == "/" && req.method="GET") {
-            res.end("index sayfası")
-        }
-        şeklinde uzun uzun yazıyorduk ama bunlara gerek yok. Routing işlemi use içerisinde kolaylaştırılmış. use nin ilk parametresine bir url verebiliyoruz  bu sayede verdiğin urldeki sayfa çalışır.
-    
-    */
 
 
