e873f0ff67fed2fa86f11c78c266f1ecd3ff5a2b
Erkan <erkankolakan@gmail.com>
2023-09-09 01:13:15 +0300

    if blogu

--- routes/user.js
+++ routes/user.js
@@ -10,21 +10,32 @@ const data =  {
             baslik:"Web Geliştirme Kursu",
             aciklama:"Node.js ile sıfırdan ileri seviye dinamik web uygulaması geliştirmeyi öğren.",
             resim:"1.jpg",
-            anasayfa:true
+            anasayfa:true,
+            durum:true
         },
         {
             blogid:2,
             baslik:"Python ile Sıfırdan İleri Seviye Python Programlama",
             aciklama:"Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
             resim:"2.jpg",
-            anasayfa:true
+            anasayfa:true,
+            durum:false
         },
         {
             blogid:3,
             baslik:"Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
             aciklama:"Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
             resim:"3.jpg",
-            anasayfa:true // burası ana sayfada olmasın diyebilmek için bu şekilde diyoruz.     
+            anasayfa:true, // burası ana sayfada olmasın diyebilmek için bu şekilde diyoruz.   
+            durum:true
+        },
+        {
+            blogid:4,
+            baslik:"Sıfırdan Uygulamalı React Geliştirme: Hooks, Redux & Firebase",
+            aciklama:"En popüler frontend kütüphanesi React'i baştan sona uygulamalı projelerle öğren. Hooks, Redux, Webpack, Firebase ve Fazlası.",
+            resim:"4.jpg",
+            anasayfa:false, // burası ana sayfada olmasın diyebilmek için bu şekilde diyoruz.     
+            durum:true
         },
     ]
 }
