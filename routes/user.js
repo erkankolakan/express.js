de3e7e06a5cfaceb73359f62491a91d3f3f0f678
Erkan <erkankolakan@gmail.com>
2023-09-09 17:16:23 +0300

    .then .catch ile kategori verilerini çektik

--- routes/user.js
+++ routes/user.js
@@ -2,82 +2,52 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/db")
 
-const data =  {
-    title:"Popüler Kurslar", 
-    categories:["Web Geliştirme","Mobil Uygulamalar","Veri Analizi","Programlama","Ofis Uygulamaları"],
-    blogs:[
-        {
-            blogid:1,
-            baslik:"Web Geliştirme Kursu",
-            aciklama:"Node.js ile sıfırdan ileri seviye dinamik web uygulaması geliştirmeyi öğren.",
-            resim:"1.jpg",
-            anasayfa:true,
-            durum:true
-        },
-        {
-            blogid:2,
-            baslik:"Python ile Sıfırdan İleri Seviye Python Programlama",
-            aciklama:"Sıfırdan İleri Seviye Python Dersleri.Veritabanı,Veri Analizi,Bot Yazımı,Web Geliştirme(Django)",
-            resim:"2.jpg",
-            anasayfa:true,
-            durum:false
-        },
-        {
-            blogid:3,
-            baslik:"Sıfırdan İleri Seviye Modern Javascript Dersleri ES7+",
-            aciklama:"Modern javascript dersleri ile (ES6 & ES7+) Nodejs, Angular, React ve VueJs için sağlam bir temel oluşturun.",
-            resim:"3.jpg",
-            anasayfa:true,
-            durum:true
-        },
-        {
-            blogid:4,
-            baslik:"Sıfırdan Uygulamalı React Geliştirme: Hooks, Redux & Firebase",
-            aciklama:"En popüler frontend kütüphanesi React'i baştan sona uygulamalı projelerle öğren. Hooks, Redux, Webpack, Firebase ve Fazlası.",
-            resim:"4.jpg",
-            anasayfa:false,
-            durum:true
-        },
-    ]
-}
-
 router.use( "/blogs/:blogid", (req ,res) => {
     res.render("users/blogDetails")
 }  )
 
 router.use( "/blogs", (req ,res) => {
     db.query("select * from blog where onay=1")
-        .then(result => {
+    .then(result => {
+
+        db.query("select * from category")
+
+            .then(sonuc => { 
+            console.log()
+
             res.render("users/blogs" , {
                 title:"Tüm Kurslar",
                 blogs:result[0], 
-                categories: data.categories
+                categories: sonuc[0] //bu bölümde kategory bilgisinide veri tabanına yazıp ordan çekmiş olduk.
             })
-        }) 
-        .catch(err => {
-            console.log(err);
-        }) 
-}  )
+
+            })
+            .catch(err => console.log(err))
+    }) 
+    .catch(err => {console.log(err)}) 
+})
+
+//burada extra bir sorgu çalıştırmış oluyoruz burası oldukça dikkat edilmesi gereken bir konu. Sorgudan gelen cevap then bloğu içerisinde ele alınıyor dolayısıyla then bloğu içerisinde extra bir sorgu çalıştırıyorum gelen bir sonuca göre diğer sonuçların çalıştırılmasını söylüyorum nede olsa asenkron bir işlemde sonucun ne zaman biteceği ne zaman geleceği belli değil dolayısıyla gelen sonucu beklememiz gerek bekle aşaması then bloğu içerisinde buradan bir cevap gelecek ve ben bu cevaba göre extra sorgu göndericem. 
+// biz burada 2 tane bilgi aldık biz extra bir bilgi daha almak istediğimiz zaman en sonuncu then bloğunun içerisine tekrar then catch aracılığıyla bir sorgu yazacağız ve ona göre de res. .then içerisine almamız gerekir böyle ard arda callbacklere callbackhell diye adlandırılır.
 
 router.use( "/", (req ,res) => {
     db.query("select * from blog where onay=1 and anasayfa=1") 
         .then(result => {
-            res.render("users/index" , {
-                title:"Popüler Kurslar",
-                blogs:result[0], 
-                categories: data.categories
-            })
+
+            db.query("select * from category")
+
+                .then(sonuc => {
+                console.log()
+
+                res.render("users/index" , {
+                    title:"Popüler Kurslar",
+                    blogs:result[0], 
+                    categories: sonuc[0]
+                })
+
+                })
+                .catch(err => console.log(err))
         }) 
-        .catch(err => {
-            console.log(err);
-        }) 
+        .catch(err => {console.log(err)}) 
 }  )
 module.exports= router
-
-// biz select * ile tüm verileri getiriyoruz, getirdikten sonra filitreleme işlemi yapıyoruz 99988989 tane veri olsa 99988989 tanesini de çekecek. Ama biz çekerken filitreleme işlemi yaparsak bize lazım olan kadar veri çekmiş olacağız. Bunun içinde
-// where = diyerek biz tablodaki veriye koşul ekliyebiliyoruz.
-
-//boolean değerin karşılığı aslında 0 1 dir -> 1 = true , 0 = false
-
-// db.query("select * from blog where onay=1 and anasayfa ")  -> iki koşuluda sağlasın istiyorsak AND
-// db.query("select * from blog where onay=1 && or anasayfa ") -> iki koşuldan biri sağlanırsa da olur demek istiyorsak OR 
