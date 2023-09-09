54962b492f761e6d27ae2e229aa4bb9459ce926a
Erkan <erkankolakan@gmail.com>
2023-09-09 15:45:03 +0300

    verileri çekerken filitreleme işlemi

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
-// MySQL e nasıl bağlanıyoruz oynat bakalım
-// uygulamamız ile veri tabanı arasındaki bağlantıyı sağlayacak bir kütüphanemiz var  -> mysql2
+// Uygulama ile server arasında bağlantıyı kurduk artık sql sorguları yapabilyoruz -> BLOG İÇİN VERİ TABANINDAN VERİ ÇEKELİM
 const express = require("express");
 const app = express(); 
 const path = require("path") 
 const userRoutes = require("./routes/user") 
 const adminRoutes = require("./routes/admin")
-const mysql = require("mysql2") //serverri uygulamaya bağlmakak için gerekli kütüphaneyi indirdik. 
-const config = require("./config")
-const connection = mysql.createConnection(config.db) //bağlanacağımız server ile bilgileri buraya gönderememiz gerekir. Bu bilgileri tüm sayfalardan erişebilmek ve sürekli aynı şeyleri tekrer tekrar yazmamak için ortak bir sayfadan erişeceğiz o sayfada config.js dosyası olacak.
 
-
-connection.connect((err) => { //burasu servere bağlanıp bağlanamadığımızı yoklayacak olan kısım
-    if (err) {
-    return console.log(err);
-    }
-    connection.query("select * from blog", (err , result) => {
-        console.log(result)
-//query methodu sayesinde bir tane sql cümleciğini yazdırabiliyoruz. Örneğin bağlanmış olduğumuz veri tabanındaki blog tablosundaki bütün kayıtları alalım. Bu sorgu çalışacak bittiğinden emin olmak için callback fonksiyonu tanımlıyoruz, fonksiyona gelen bilgiler err ve resul (hata ve cevap )
-//bize veri tabanındaki tüm ojeler gelir eğer ben ilk objeyi almak istiyorsam result[0] diyerek index numarası ile çağırabilirim. result[0].title yazark da başlık bilgisini alabiliriz.
-    })   
-    console.log("Bağlantı başarılı");
-/* Bu şekilde kullandık ve else yok,->
-Bu şekilde kullandığımız zaman if içinde retun var retun gerçekten değeri döndürürse bir alt satıra geçmeden direk fonksiyondaki işlemi bitirir. Eğer retun çalışmasada otomatik bir şekilde console.log("Bağlantı başarılı") yazınısını consoleye yazdıracaktır. */
-})
+//data bilgilerini componenet giib başka sayfada oluşuturp öyle require yapıyoruz ki her sayfa veri bilgilerine erişebilsin erişebilsin
 
 app.set("view engine","ejs")
 
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../data/db")
 
 const data =  {
     title:"Popüler Kurslar", 
@@ -40,18 +41,43 @@ const data =  {
     ]
 }
 
-
 router.use( "/blogs/:blogid", (req ,res) => {
     res.render("users/blogDetails")
 }  )
 
-
 router.use( "/blogs", (req ,res) => {
-    res.render("users/blogs", data) 
+    db.query("select * from blog where onay=1")
+        .then(result => {
+            res.render("users/blogs" , {
+                title:"Tüm Kurslar",
+                blogs:result[0], 
+                categories: data.categories
+            })
+        }) 
+        .catch(err => {
+            console.log(err);
+        }) 
 }  )
 
 router.use( "/", (req ,res) => {
-    res.render("users/index" , data)
+    db.query("select * from blog where onay=1 and anasayfa=1") 
+        .then(result => {
+            res.render("users/index" , {
+                title:"Popüler Kurslar",
+                blogs:result[0], 
+                categories: data.categories
+            })
+        }) 
+        .catch(err => {
+            console.log(err);
+        }) 
 }  )
-
 module.exports= router
+
+// biz select * ile tüm verileri getiriyoruz, getirdikten sonra filitreleme işlemi yapıyoruz 99988989 tane veri olsa 99988989 tanesini de çekecek. Ama biz çekerken filitreleme işlemi yaparsak bize lazım olan kadar veri çekmiş olacağız. Bunun içinde
+// where = diyerek biz tablodaki veriye koşul ekliyebiliyoruz.
+
+//boolean değerin karşılığı aslında 0 1 dir -> 1 = true , 0 = false
+
+// db.query("select * from blog where onay=1 and anasayfa ")  -> iki koşuluda sağlasın istiyorsak AND
+// db.query("select * from blog where onay=1 && or anasayfa ") -> iki koşuldan biri sağlanırsa da olur demek istiyorsak OR 
