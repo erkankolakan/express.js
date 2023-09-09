f97ec28bd7d33e5cdc8835a7a4d4186e3365ccc5
Erkan <erkankolakan@gmail.com>
2023-09-09 20:15:42 +0300

    id numarasına göre detail sayfası getirme

--- routes/user.js
+++ routes/user.js
@@ -2,52 +2,59 @@ const express = require("express");
 const router = express.Router();
 const db = require("../data/db")
 
-router.use( "/blogs/:blogid", (req ,res) => {
-    res.render("users/blogDetails")
+router.use( "/blogs/:blogid", async(req ,res) => {
+
+    const id = req.params.blogid //url deki blogid kısımına yazılan id değerini alır oldukça önemlidir. Gelen bilgi olduğu için req le alıyoruz.
+
+    try {
+        const [blog, ] = await db.query("select * from blog where blogid=?", [id])
+        // const [blogs, ] = await db.query("select * blog where blogid=? and baslik=?", [id , baslik]) eğer iki tane belirleyicimiz olsa bu şekilde yapardık.
+
+        if (blog[0]) { //bu şekilde yapmamızın sebebi bizde sadece 4 tane id var kullanıcı gidip url de 4651531 yazarsa uygulama takılır biz takılmasını istemediğimiz için geçersiz bir blogid değeri geldiği zaman bizi direk index atsın diye
+            return res.render("users/blogDetails" , {
+            title: blog[0].baslik, //bunu direk blogDetail sayfasında kullanmasakda parçalar klasöründe head kullanıyor o yüzden göndermemiz gerek
+            blog: blog[0], //blog[0] ı blogDetails sayfasına blog adıyla gönderdir şimdi blogDetails dan karşılaman gerekir 
+        })}
+        res.redirect("/") //retun a girerse onun aşağısındaki kodlar çalışmayacağı için gönül rahatlığıyla bu şekilde yazabiliriz 
+
+    } catch (error) {
+        console.log(error);
+    }
+
 }  )
 
-router.use( "/blogs", (req ,res) => {
-    db.query("select * from blog where onay=1")
-    .then(result => {
+router.use( "/blogs", async(req ,res) => {
+    try{
+        const [blogs, ] = await db.query("select * from blog where onay=1")
+        const [categories, ] = await db.query("select * from category") 
 
-        db.query("select * from category")
+        res.render("users/blogs" , {
+            title:"Tüm Kurslar",
+            blogs:blogs, 
+            categories: categories 
+        })
+    }
 
-            .then(sonuc => { 
-            console.log()
-
-            res.render("users/blogs" , {
-                title:"Tüm Kurslar",
-                blogs:result[0], 
-                categories: sonuc[0] //bu bölümde kategory bilgisinide veri tabanına yazıp ordan çekmiş olduk.
-            })
-
-            })
-            .catch(err => console.log(err))
-    }) 
-    .catch(err => {console.log(err)}) 
+    catch(err){
+        console.log(err);
+    }
 })
 
-//burada extra bir sorgu çalıştırmış oluyoruz burası oldukça dikkat edilmesi gereken bir konu. Sorgudan gelen cevap then bloğu içerisinde ele alınıyor dolayısıyla then bloğu içerisinde extra bir sorgu çalıştırıyorum gelen bir sonuca göre diğer sonuçların çalıştırılmasını söylüyorum nede olsa asenkron bir işlemde sonucun ne zaman biteceği ne zaman geleceği belli değil dolayısıyla gelen sonucu beklememiz gerek bekle aşaması then bloğu içerisinde buradan bir cevap gelecek ve ben bu cevaba göre extra sorgu göndericem. 
-// biz burada 2 tane bilgi aldık biz extra bir bilgi daha almak istediğimiz zaman en sonuncu then bloğunun içerisine tekrar then catch aracılığıyla bir sorgu yazacağız ve ona göre de res. .then içerisine almamız gerekir böyle ard arda callbacklere callbackhell diye adlandırılır.
+router.use( "/", async(req ,res) => {
 
-router.use( "/", (req ,res) => {
-    db.query("select * from blog where onay=1 and anasayfa=1") 
-        .then(result => {
+    try{
+        const [blogs, ] = await db.query("select * from blog where onay=1 and anasayfa=1")
+        const [categories, ] = await db.query("select * from category")
+        
+        res.render("users/index" , {
+            title:"Tüm Kurslar",
+            blogs:blogs, 
+            categories:categories
+        })
+    }
+    catch(err){
+        console.log(err)
+    }
 
-            db.query("select * from category")
-
-                .then(sonuc => {
-                console.log()
-
-                res.render("users/index" , {
-                    title:"Popüler Kurslar",
-                    blogs:result[0], 
-                    categories: sonuc[0]
-                })
-
-                })
-                .catch(err => console.log(err))
-        }) 
-        .catch(err => {console.log(err)}) 
 }  )
 module.exports= router
