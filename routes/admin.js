cd35f912c507cfc802f9bcbacc32938e39ae6b50
Erkan <erkankolakan@gmail.com>
2023-09-10 17:34:35 +0300

    ADMİNİN dinamic kategoriler

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const db = require("../data/db")
 
 
-router.use( "/blog/create", (req ,res) => {
-    res.render("admin/blog-create")
+router.use( "/blog/create", async(req ,res) => {
+
+    try {
+/* bize gelen veride ilk değer veri ile alakalı diğer ikinci değer ise kolon bilgileri bizim şuan kolon bilgileri ile işimiz yok o yüzden bizde ilk değerleri alabilek adına [ xxxxxxx ,] şekliden yazıyoruz 
+*/
+        const [categories, ] = await db.query("select * from category") //kategori tablosunda ki tüm verileri getir
+        res.render("admin/blog-create" , {
+            title: "Yeni Blog Ekle",
+            categories: categories,  //üste almış olduğumuz veriyi sayfaya gönderiyoruz, blog-create sayfasında da categories ismiyle verileri karşılayacağız.
+        })
+        
+    } catch (error) {
+        console.log(error);
+    }
+
+
 }  )
 
 router.use( "/blog/:blogid", (req ,res) => {
