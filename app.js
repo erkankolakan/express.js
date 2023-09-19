772f0dd62f6109a59479315aed8778455566a93e
Erkan <erkankolakan@gmail.com>
2023-09-19 14:53:12 +0300

    sessionu veri tabanında saklamak

--- app.js
+++ app.js
@@ -1,42 +1,61 @@
+//express
 const express = require("express");
 const app = express(); 
+
+const cookieParser = require("cookie-parser") //cookie
+const session = require("express-session") //session
+const SequelizeStroe = require("connect-session-sequelize")(session.Store)
+
+//node modules
 const path = require("path") 
+
+//routes
 const userRoutes = require("./routes/user") 
 const adminRoutes = require("./routes/admin")
 const authRoutes = require("./routes/auth")
-const cookieParser = require("cookie-parser") //cookie
-const session = require("express-session") //session
 
+//custom modules
+const sequelize = require("./data/db") 
+const dummyData = require("./data/dummy-data") 
+
+//tamplade engine
+app.set("view engine","ejs")
+
+//models
+const Category = require("./models/category")
+const Blog = require("./models/blog")
+const User = require("./models/user")   
+
+
+//middleware
+app.use(express.urlencoded())
 app.use(cookieParser())
 
-app.use(session({
+app.use(session({                    
     secret:"0ade738c-2251-433f-8482-68ae2c6aa6b6", 
     resave: true, 
     saveUninitialized: false, 
     cookie:{ 
-        maxAge: 1000 * 60 * 60 * 24
-    }
+        maxAge: 1000*60*60*24         // dosya hiyerarşisi önemlidir üstde yazmış olsak hata alırdık 
+    },
+    store: new SequelizeStroe({
+        db: sequelize
+    })
 }));
+/*
+Artık oluşturulmuş olan session id nin karşılığı veri tabanında. Yani ben serverı durdurşmuş olsam bile uygulama içinde gezebilirim. Çünkü benim uygulama içinde gezebilmemi sağlayan izin bilgileri veri tabanında.
+*/ 
 
-
-app.use(express.urlencoded())
-
-app.set("view engine","ejs")
 
 app.use("/libs" , express.static(path.join(__dirname,"node_modules")))
 app.use("/static", express.static(path.join(__dirname,"public")))    
 
-    app.use("/admin",adminRoutes); 
-    
-    app.use("/account", authRoutes);  
-    
-    app.use(userRoutes); 
 
-    const sequelize = require("./data/db") 
-    const dummyData = require("./data/dummy-data") 
-    const Category = require("./models/category")
-    const Blog = require("./models/blog")
-    const User = require("./models/user")   
+app.use("/admin",adminRoutes); 
+app.use("/account", authRoutes);  
+    
+app.use(userRoutes); 
+
 
     Blog.belongsTo(User); 
     User.hasMany(Blog); 
@@ -45,13 +64,18 @@ app.use("/static", express.static(path.join(__dirname,"public")))
     Category.belongsToMany(Blog , {through: "blogCategories"});
 
 (async () => {
-    await sequelize.sync({force: true});
-    await dummyData()
+    // await sequelize.sync({force: true});
+    // await dummyData()
 })();
+
 
     app.listen(3000 , () => {
         console.log("Server started on port 3000");
     })
+
+
+
+
 
         // //ilişkiler
     // //one to many (bire çok ilişki)
